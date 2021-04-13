5df470bdc3737cdb0724629203deaee5aa28c59d
Stefano <tepo@hotmail.it>
2021-04-13 10:33:38 +0200

    Aggiunta ricerca per serie tv

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,13 @@ var app = new Vue(
     el: "#app",
     data: {
       search: "",
-      movieList: []
+      movieList: [],
+      showsList: [],
+      movieTempList: []
     },
     methods: {
       find: function () {
+        this.movieList = [];
         if ( this.search != "" ) {
           axios.get("https://api.themoviedb.org/3/search/movie", {
             params: {
@@ -16,15 +19,42 @@ var app = new Vue(
             }
           })
           .then( (response) => {
-            this.movieList = response.data.results;
-            this.movieList.forEach((movie) => {
-              movie.stars = Math.ceil(movie.vote_average / 2);
-              if ( movie.original_language == "en" ) {
-                movie.original_language = "us";
-              }
+            this.movieTempList = response.data.results;
+            this.setLanguageAndStars(this.movieTempList);
+            this.movieTempList.forEach((movie) => {
+              movie.type = "movie";
+              this.movieList.push(movie);
+            });
+          });
+
+          axios.get("https://api.themoviedb.org/3/search/tv", {
+            params: {
+              api_key: "2919db319a3e21c76ac6ca90eacd6463",
+              query: this.search,
+              language: "it-IT"
+            }
+          })
+          .then( (response) => {
+            this.showsList = response.data.results;
+            this.setLanguageAndStars(this.showsList);
+            this.showsList.forEach((show, i) => {
+              show.type = "series";
+              this.movieList.push(show);
             });
           });
         }
+      },
+
+      setLanguageAndStars: function(element) {
+        element.forEach((item, i) => {
+          item.stars = Math.ceil(item.vote_average / 2);
+          if ( item.original_language == "en" ) {
+            item.original_language = "us";
+          } else if ( item.original_language == "ja" ) {
+            item.original_language = "jp";
+          }
+        });
+
       }
     }
   }
