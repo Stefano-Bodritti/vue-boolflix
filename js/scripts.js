28a2d19ab6b835bb275305899c744c19deae195a
Stefano <tepo@hotmail.it>
2021-04-15 09:36:41 +0200

    Refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -13,28 +13,28 @@ var app = new Vue(
         this.searchList = [];
         if ( this.search != "" ) {
           if ( this.selected == "all" ) {
-            this.searchMovie();
-            this.searchSeries();
+            this.searchMovie("movie");
+            this.searchMovie("tv");
           } else if ( this.selected == "movie" ) {
-            this.searchMovie();
+            this.searchMovie("movie");
           } else if ( this.selected == "series" ) {
-            this.searchSeries();
+            this.searchMovie("tv");
           }
         }
       },
 
-      setLanguageAndStars: function(element) {
-        element.forEach((item, i) => {
-          item.stars = Math.ceil(item.vote_average / 2);
-          if ( item.original_language == "en" ) {
-            item.original_language = "us";
-          } else if ( item.original_language == "ja" ) {
-            item.original_language = "jp";
+      setLanguageAndStars: function(movie) {
+        // element.forEach((item, i) => {
+          movie.stars = Math.ceil(movie.vote_average / 2);
+          if ( movie.original_language == "en" ) {
+            movie.original_language = "us";
+          } else if ( movie.original_language == "ja" ) {
+            movie.original_language = "jp";
           }
-        });
+        // });
       },
-      searchMovie: function () {
-        axios.get("https://api.themoviedb.org/3/search/movie", {
+      searchMovie: function (category) {
+        axios.get("https://api.themoviedb.org/3/search/" + category, {
           params: {
             api_key: "2919db319a3e21c76ac6ca90eacd6463",
             query: this.search,
@@ -43,32 +43,32 @@ var app = new Vue(
         })
         .then( (response) => {
           this.movieList = response.data.results;
-          this.setLanguageAndStars(this.movieList);
           this.movieList.forEach((movie) => {
-            movie.type = "movie";
+            this.setLanguageAndStars(movie);
+            movie.type = category;
             this.searchList.push(movie);
           });
           this.searchList.sort( (a, b) => b.vote_count - a.vote_count);
         });
       },
-      searchSeries: function () {
-        axios.get("https://api.themoviedb.org/3/search/tv", {
-          params: {
-            api_key: "2919db319a3e21c76ac6ca90eacd6463",
-            query: this.search,
-            language: "it-IT"
-          }
-        })
-        .then( (response) => {
-          this.showsList = response.data.results;
-          this.setLanguageAndStars(this.showsList);
-          this.showsList.forEach((show, i) => {
-            show.type = "series";
-            this.searchList.push(show);
-          });
-          this.searchList.sort( (a, b) => b.vote_count - a.vote_count);
-        });
-      }
+      // searchSeries: function () {
+      //   axios.get("https://api.themoviedb.org/3/search/tv", {
+      //     params: {
+      //       api_key: "2919db319a3e21c76ac6ca90eacd6463",
+      //       query: this.search,
+      //       language: "it-IT"
+      //     }
+      //   })
+      //   .then( (response) => {
+      //     this.showsList = response.data.results;
+      //     this.setLanguageAndStars(this.showsList);
+      //     this.showsList.forEach((show, i) => {
+      //       show.type = "series";
+      //       this.searchList.push(show);
+      //     });
+      //     this.searchList.sort( (a, b) => b.vote_count - a.vote_count);
+      //   });
+      // }
     }
   }
 );
